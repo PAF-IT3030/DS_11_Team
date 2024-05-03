2685af3d984649045ee056f0defeeb28072a8711
IT21227486 <it21227486@my.sliit.lk>
2024-05-03 21:21:10 +0530

    modified repository  one

--- backend/src/main/java/com/fithub/backend/mealplan/repository/MealPlanPostRepository.java
+++ backend/src/main/java/com/fithub/backend/mealplan/repository/MealPlanPostRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MealPlanPostRepository extends JpaRepository<MealPlanPost, Long> {  
+public interface MealPlanPostRepository extends JpaRepository<MealPlanPost, Long> {   
 }
--- backend/src/main/java/com/fithub/backend/mealplan/repository/MealPlanRepository.java
+++ backend/src/main/java/com/fithub/backend/mealplan/repository/MealPlanRepository.java
@@ -1,7 +1,7 @@
 package com.fithub.backend.mealplan.repository;
 
 import com.fithub.backend.mealplan.entity.MealPlan;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaRepository; 
 import org.springframework.stereotype.Repository;
 
 @Repository
