02605cb0c112b4e7ffef7c3beed29dbbe1088ccc
IT21227486 <it21227486@my.sliit.lk>
2024-05-03 21:15:51 +0530

    modified entity one

--- backend/src/main/java/com/fithub/backend/mealplan/entity/MealPlan.java
+++ backend/src/main/java/com/fithub/backend/mealplan/entity/MealPlan.java
@@ -13,7 +13,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-public class MealPlan {
+public class MealPlan {  
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
