ba4befe42a9903976b2010cda862c236eed71d77
duudi <it21254970@my.sliit.lk>
2024-05-04 10:20:02 +0530

    error correction

--- backend/src/main/java/com/fithub/backend/workoutplan/repository/WorkoutPlanPostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fithub.backend.workoutplan.repository;
-
-import com.fithub.backend.workoutplan.entity.WorkoutPlanPost;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface WorkoutPlanPostRepository extends JpaRepository<WorkoutPlanPost, Long> {
-}
--- backend/src/main/java/com/fithub/backend/workoutplan/repository/WorkoutPlanRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fithub.backend.workoutplan.repository;
-
-import com.fithub.backend.workoutplan.entity.WorkoutPlan;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface WorkoutPlanRepository extends JpaRepository<WorkoutPlan, Long> {
-}
